277022b30bca694ff1ed45ebc9b2aac9640dccdd
Vahe Ghazaryan <vaheghazaryan122@gmail.com>
2025-08-25 17:51:25 +0400

    Delete Chess directory

--- Chess/ts/globalTypes.ts
+++
@@ -1,7 +0,0 @@
-import { ChessPiece } from "./chessPiece/chessPiece";
-
-type typePlaceColor = "white" | "black";
-type typePiece = "pawn" | "rook" | "knight" | "bishop" | "queen" | "king";
-type position = { row: number, col: number };
-
-export type { typePlaceColor, typePiece, position };
