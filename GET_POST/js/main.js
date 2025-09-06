289b1ad2ba3da863093cfb9ca219185a4ee11557
Vahe Ghazaryan <vaheghazaryan122@gmail.com>
2025-09-06 12:56:01 +0400

    Add files via upload

--- GET_POST/js/main.js
+++ GET_POST/js/main.js
@@ -1,24 +1,3 @@
-// fetch("http://localhost:3000", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-//         fullName: "Vahe Ghazaryan",
-//         email: "vaheghazaryan122@gmail.com",
-//         age: 20,
-//     })
-// })
-
-// fetch("http://localhost:3000", {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// }).then(res => res.json()).then(data => {
-//     console.log(data)
-// }).catch(err => console.log(err))
-
 const registerBtn = document.getElementById("registerBtn");
 registerBtn.addEventListener("click", (e) => {
     e.preventDefault();
