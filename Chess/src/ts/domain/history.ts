71d2835eed6d842046d6b9d57b2c0ab9b215857d
Vahe Ghazaryan <vaheghazaryan122@gmail.com>
2025-09-26 23:36:26 +0400

    fix

--- Chess/src/ts/domain/history.ts
+++ Chess/src/ts/domain/history.ts
@@ -7,7 +7,7 @@ export class History {
         let o = (String.fromCharCode("A".charCodeAt(0) + oldPos.col)) + "" + (oldPos.row + 1);
         let n = (String.fromCharCode("A".charCodeAt(0) + newPos.col)) + "" + (newPos.row + 1);
 
-        MovesList!.innerHTML += `<p>${o} move ${n}</p>`
+        MovesList!.innerHTML = `<p>${o} move ${n}</p>` + MovesList!.innerHTML;
         const movesCount = document.getElementById("movesCount")
         if (movesCount) {
             let tmp = +movesCount.textContent;
